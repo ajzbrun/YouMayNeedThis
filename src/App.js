e92fe4b6c31bf8d68aca5bf78281428fcb7f958a
azbrun <ajzbrun@gmail.com>
2021-09-22 21:57:23 -0300

    navbar ok

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap';
+		
+//components
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>YouMayNeedThis</h1>
     </div>
   );
